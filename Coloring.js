4c4240367956b80a68f14efc9ebf8aa87923c6e0
Alexey Shein <lexx918@mail.ru>
2015-08-03 17:53:06 +0300

    async pre-loader

--- Coloring.js
+++ Coloring.js
@@ -1,24 +1,19 @@
 /**
  *
  */
-function Coloring(img, width, height)
+function Coloring(canvas, width, height, progressCallback)
 {
-    var img = img,
-        width = width,
-        height = height,
-        backgroundColor = [255,255,255],
-        borderColor = [0,0,0];
+    var canvas = canvas,
+        width  = width,
+        height = height
+        progressCallback = progressCallback;
 
-    var debug = function(){
-        for (var str, row = 0; row < height; row++) {
-            str = ""+row+": ";
-            for (var i = 0, len = lines[row].length; i < len; i++) {
-                str += "["+lines[row][i][0]+","+lines[row][i][1]+","+lines[row][i][2]+"], ";
-            }
-            console.log(str);
-            str = "";
-        }
-    }
+    var background = [255,255,255],
+        border     = [  0,  0,  0],
+        error      = 220
+        imageData  = canvas.getImageData(0, 0, width, height).data;
+
+    var init = false;
 
     /**
      * Extraction lines
@@ -28,20 +23,22 @@ function Coloring(img, width, height)
      *   rowNum => [ [leftX, rightX, claster], [..], [..] ],
      * ]
      */
-    var lines = [],
-        imageData = img.getImageData(0, 0, width, height).data,
-        canFill = function(x, y){
-            var offset = (y*width + x) * 4,
-                dot = [imageData[offset], imageData[offset + 1], imageData[offset + 2]],
-                error = 220;
-            return ""+dot !== ""+borderColor &&
-                Math.abs(backgroundColor[0] - dot[0]) <= error &&
-                Math.abs(backgroundColor[1] - dot[1]) <= error &&
-                Math.abs(backgroundColor[2] - dot[2]) <= error;
-        };
-    for (var left, row = 0; row < height; row++) {
+    var lines = [];
+    var getDot = function(x, y){
+        var offset = (y*width + x) * 4;
+        return [imageData[offset], imageData[offset + 1], imageData[offset + 2]];
+    }
+    var canFill = function(x, y){
+        var dot = getDot(x, y);
+        return ""+dot !== ""+border &&
+            Math.abs(background[0] - dot[0]) <= error &&
+            Math.abs(background[1] - dot[1]) <= error &&
+            Math.abs(background[2] - dot[2]) <= error;
+    }
+    var row = 0;
+    var extract = function(){
         lines[row] = [];
-        left = -1;
+        var left = -1;
         for (var col = 0; col < width; col++) {
             if (canFill(col, row)) {
                 if (left === -1) {
@@ -57,16 +54,26 @@ function Coloring(img, width, height)
         if (left !== -1) {
             lines[row].push([left, width, 0]);
         }
+
+        if (++row < height) {
+            progressCallback((row*50)/height);
+            setTimeout(extract, 0);
+        } else {
+            row = 0;
+            clustering();
+        }
     }
-    imageData = null;
+    setTimeout(extract, 0);
 
     // clustering
+    var claster = 0;
+    var child, childrenUp, childrenDown, children = [];
     var getChildren = function(left, right, row){
         if (row < 0 || row >= height) {
             return;
         }
 
-        var children = [];
+        var arr = [];
         // http://www.codeproject.com/Articles/6017/QuickFill-An-efficient-flood-fill-algorithm
         for (var i = 0, len = lines[row].length; i < len; i++) {
             if (
@@ -78,13 +85,13 @@ function Coloring(img, width, height)
                     lines[row][i][1] > right && lines[row][i][0] <= right
                 )
             ) {
-                children.push([row, i]);
+                arr.push([row, i]);
                 left = lines[row][i][1] + 2;
             }
         }
-        return children;
-    };
-    for (var child, childrenUp, childrenDown, children = [], claster = row = 0; row < height; row++) {
+        return arr;
+    }
+    var clustering = function(){
         for (var i = 0, len = lines[row].length; i < len; i++) {
             if (lines[row][i][2] === 0) {
                 claster++;
@@ -101,12 +108,23 @@ function Coloring(img, width, height)
                 }
             }
         }
-    };
+
+        if (++row < height) {
+            progressCallback(50 + (row*50)/height);
+            setTimeout(clustering, 0);
+        } else {
+            init = true;
+            console.log("init = true");
+        }
+    }
 
     this.fill = function(x, y, color){
+        if (!init) {
+            return;
+        }
+
         var row = y,
             left = x;
-
         if (row < 0 || row >= height) {
             return;
         }
@@ -122,25 +140,30 @@ function Coloring(img, width, height)
             return;
         }
 
-        img.fillStyle = "rgb("+color+")";
+        canvas.fillStyle = "rgb("+color+")";
 
-        // @TODO optimize!!!
-        for (var row = 0; row < height; row++) {
-            for (var i = 0, len = lines[row].length; i < len; i++) {
-                if (lines[row][i][2] === claster) {
-                    img.fillRect(lines[row][i][0], row, lines[row][i][1] - lines[row][i][0] + 1, 1);
+        var fillRow = function(r){
+            var found = false;
+            for (var i = 0, len = lines[r].length; i < len; i++) {
+                if (lines[r][i][2] === claster) {
+                    found = true;
+                    canvas.fillRect(lines[r][i][0], r, lines[r][i][1] - lines[r][i][0] + 1, 1);
+                }
+            }
+            return found;
+        }
+
+        for (var r = row; r < height; r++) {
+            if (!fillRow(r)) {
+                break;
+            }
+        }
+        if (row > 0) {
+            for (var found, r = row - 1; r >= 0; r--) {
+                if (!fillRow(r)) {
+                    break;
                 }
             }
         }
-    }
-
-    /* debug */
-    this.next = function(){
-        tick();
-    }
-    this.dot = function(x,y){
-        imageData = img.getImageData(0, 0, width, height).data;
-        var offset = (y*width + x) * 4;
-        return [imageData[offset], imageData[offset + 1], imageData[offset + 2]];
     }
 }
